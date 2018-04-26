019286ac0eb35bfff37b364675f358768925672e
Jacopo De Luca <jacopo.delu@gmail.com>
2018-04-26 11:16:41 +0200

    refactoring

--- support/elf.py
+++ support/elf.py
@@ -1,146 +1,111 @@
-import io
-
-# DataType
-ELF32_ADDR = 4
-ELF32_HALF = 2
-ELF32_OFF = 4
-ELF32_SWORD = 4
-ELF32_WORD = 4
-ELF32_LWORD = 8
-
-# DataType 64
-ELF64_ADDR = 8
-ELF64_HALF = 2
-ELF64_OFF = 8
-ELF64_SWORD = 4
-ELF64_SXWORD = 8
-ELF64_WORD = 4
-ELF64_LWORD = 8
-ELF64_XWORD = 8
+from support.cstruct import *
 
 # MAGIC
 ELF_MAGIC = bytes([0x7f, 0x45, 0x4c, 0x46])
 
+# *****************************************
+# * HEADER                                *
+# *****************************************
+EI_NIDENT = 16
+EI_MAG0 = 0x00
+EI_MAG1 = 0x01
+EI_MAG2 = 0x02
+EI_MAG3 = 0x03
+EI_CLASS = 0x04
+# *****************
+EI_CLASSNONE = 0x00
+EI_CLASS32 = 0x01
+EI_CLASS64 = 0x02
+# *****************
+EI_DATA = 0x05
+# *****************
+EI_DATANONE = 0x00
+EI_DATA2LSB = 0x01
+EI_DATA2MSB = 0x02
+# *****************
+EI_VERSION = 0x06
+EI_OSABI = 0x07
+EI_ABIVERSION = 0x08
+EI_PAD = 0x09
+
+# *****************************************
+# * SHDR                                  *
+# *****************************************
+SHT_TYPE_NULL = 0x00
+SHT_TYPE_PROGBITS = 0x01
+SHT_TYPE_SYMTAB = 0x02
+SHT_TYPE_STRTAB = 0x03
+SHT_TYPE_RELA = 0x04
+SHT_TYPE_HASH = 0x05
+SHT_TYPE_DYNAMIC = 0x06
+SHT_TYPE_NOTE = 0x07
+SHT_TYPE_NOBITS = 0x08
+SHT_TYPE_REL = 0x09
+SHT_TYPE_SHLIB = 0x0A
+SHT_TYPE_DYNSYM = 0x0B
+SHT_TYPE_LOPROC = 0x70000000
+SHT_TYPE_HIPROC = 0x7FFFFFFF
+SHT_TYPE_LOUSER = 0x80000000
+SHT_TYPE_HIUSER = 0xFFFFFFFF
+# Flags
+SHT_FLAGS_WRITE = 0x01
+SHT_FLAGS_ALLOC = 0x02
+SHT_FLAGS_EXECINSTR = 0x04
+SHT_FLAGS_MASKPROC = 0xF0000000
+
 
 class ElfHeader(object):
-    NIDENT = 16
-    MAG0 = 0x00
-    MAG1 = 0x01
-    MAG2 = 0x02
-    MAG3 = 0x03
-    CLASS = 0x04
-    DATA = 0x05
-    VERSION = 0x06
-    OSABI = 0x07
-    ABIVERSION = 0x08
-    PAD = 0x09
-
-    # Class
-    CLASSNONE = 0x00
-    CLASS32 = 0x01
-    CLASS64 = 0x02
-
-    # Encoding
-    DATANONE = 0x00
-    DATA2LSB = 0x01
-    DATA2MSB = 0x02
-
-    def __init__(self, stream: io.RawIOBase):
-        self.e_ident = bytes(ElfHeader.NIDENT)
-        self.e_type = 0
-        self.e_machine = 0
-        self.e_version = 0
-        self.e_entry = 0
-        self.e_phoff = 0
-        self.e_shoff = 0
-        self.e_flags = 0
-        self.e_ehsize = 0
-        self.e_phentsize = 0
-        self.e_phnum = 0
-        self.e_shentsize = 0
-        self.e_shnum = 0
-        self.e_shstrnd = 0
-
-        self.e_ident = stream.read(ElfHeader.NIDENT)
-        if self.wordsz == 32:
-            self.__parse32(stream, self.endianness)
-        elif self.wordsz == 64:
-            self.__parse64(stream, self.endianness)
-
-    def __parse32(self, stream: io.RawIOBase, endianness):
-        self.e_type = int.from_bytes(stream.read(ELF32_HALF), byteorder=endianness)
-        self.e_machine = int.from_bytes(stream.read(ELF32_HALF), byteorder=endianness)
-        self.e_version = int.from_bytes(stream.read(ELF32_WORD), byteorder=endianness)
-        self.e_entry = int.from_bytes(stream.read(ELF32_ADDR), byteorder=endianness)
-        self.e_phoff = int.from_bytes(stream.read(ELF32_OFF), byteorder=endianness)
-        self.e_shoff = int.from_bytes(stream.read(ELF32_OFF), byteorder=endianness)
-        self.e_flags = int.from_bytes(stream.read(ELF32_WORD), byteorder=endianness)
-        self.e_ehsize = int.from_bytes(stream.read(ELF32_HALF), byteorder=endianness)
-        self.e_phentsize = int.from_bytes(stream.read(ELF32_HALF), byteorder=endianness)
-        self.e_phnum = int.from_bytes(stream.read(ELF32_HALF), byteorder=endianness)
-        self.e_shentsize = int.from_bytes(stream.read(ELF32_HALF), byteorder=endianness)
-        self.e_shnum = int.from_bytes(stream.read(ELF32_HALF), byteorder=endianness)
-        self.e_shstrnd = int.from_bytes(stream.read(ELF32_HALF), byteorder=endianness)
-
-    def __parse64(self, stream: io.RawIOBase, endianness):
-        self.e_type = int.from_bytes(stream.read(ELF64_HALF), byteorder=endianness)
-        self.e_machine = int.from_bytes(stream.read(ELF64_HALF), byteorder=endianness)
-        self.e_version = int.from_bytes(stream.read(ELF64_WORD), byteorder=endianness)
-        self.e_entry = int.from_bytes(stream.read(ELF64_ADDR), byteorder=endianness)
-        self.e_phoff = int.from_bytes(stream.read(ELF64_OFF), byteorder=endianness)
-        self.e_shoff = int.from_bytes(stream.read(ELF64_OFF), byteorder=endianness)
-        self.e_flags = int.from_bytes(stream.read(ELF64_WORD), byteorder=endianness)
-        self.e_ehsize = int.from_bytes(stream.read(ELF64_HALF), byteorder=endianness)
-        self.e_phentsize = int.from_bytes(stream.read(ELF64_HALF), byteorder=endianness)
-        self.e_phnum = int.from_bytes(stream.read(ELF64_HALF), byteorder=endianness)
-        self.e_shentsize = int.from_bytes(stream.read(ELF64_HALF), byteorder=endianness)
-        self.e_shnum = int.from_bytes(stream.read(ELF64_HALF), byteorder=endianness)
-        self.e_shstrnd = int.from_bytes(stream.read(ELF64_HALF), byteorder=endianness)
+    e_ident = []
+    e_type = 0
+    e_machine = 0
+    e_version = 0
+    e_entry = 0
+    e_phoff = 0
+    e_shoff = 0
+    e_flags = 0
+    e_ehsize = 0
+    e_phentsize = 0
+    e_phnum = 0
+    e_shentsize = 0
+    e_shnum = 0
+    e_shstrnd = 0
 
     def __str__(self):
         return '\n'.join(['ELF Header',
                           'Magic:                       %s',
                           'ABI:                         %s',
-                          'Type:                        {e_type} (%s)',
-                          'Machine:                     {e_machine} (%s)',
+                          'Type:                        {e_type:#} (%s)',
+                          'Machine:                     {e_machine:#} (%s)',
                           'Endianness and word size:    %s endian - %d bit',
                           'Version:                     {e_version}',
                           'Entry point:                 {e_entry:#x}',
                           'Program header offset:       {e_phoff} - {e_phoff:#x} (bytes in file)',
                           'Section header offset:       {e_shoff} - {e_shoff:#x} (bytes in file)',
-                          'Flags:                       {e_flags}',
+                          'Flags:                       {e_flags:#}',
                           'Size of this header:         {e_ehsize}',
                           'Size of program headers:     {e_phentsize}',
                           'Number of program headers:   {e_phnum}',
                           'Size of section headers:     {e_shentsize}',
                           'Number of section headers:   {e_shnum}',
-                          'String table index:          {e_shstrnd}']) \
+                          'String table index:          {e_shstrnd:#}']) \
                    .format(**self.__dict__) % (" ".join([("%02x" % x).upper() for x in self.e_ident]),
                                                self.abi,
                                                self.type_str(),
                                                self.em_str(),
                                                self.endianness,
-                                               self.wordsz)
+                                               32 if self.e_ident[EI_CLASS] == EI_CLASS32 else 64)
 
     @property
     def abi(self):
-        abi = self.e_ident[ElfHeader.OSABI]
-        val = {0x00: 'System V',
-               0x01: 'HP-UX operating system',
-               0xFF: 'Standalone (embedded) application'}
+        abi = self.e_ident[EI_OSABI]
+        val = {0x00: "System V",
+               0x01: "HP-UX operating system",
+               0xFF: "Standalone (embedded) application"}
         return "Unknown: %02x" % abi if abi not in val else val[abi]
 
     @property
     def endianness(self):
-        return 'little' if self.e_ident[ElfHeader.DATA] == ElfHeader.DATA2LSB else 'big'
-
-    @property
-    def wordsz(self):
-        if self.e_ident[ElfHeader.CLASS] == ElfHeader.CLASS32:
-            return 32
-        elif self.e_ident[ElfHeader.CLASS] == ElfHeader.CLASS64:
-            return 64
-        raise TypeError("Not a valid ELF")
+        return "big" if self.e_ident[EI_DATA] == EI_DATA2MSB else "little"
 
     def type_str(self):
         val = {0x00: "No file type",
@@ -171,113 +136,139 @@ class ElfHeader(object):
         return "Unknown: %02x" % self.e_machine if self.e_machine not in val else val[self.e_machine]
 
 
+class ElfHeader32(ElfHeader, metaclass=CStruct):
+    e_ident = "16s"
+    e_type = USHORT
+    e_machine = USHORT
+    e_version = UINT
+    e_entry = UINT
+    e_phoff = UINT
+    e_shoff = UINT
+    e_flags = UINT
+    e_ehsize = USHORT
+    e_phentsize = USHORT
+    e_phnum = USHORT
+    e_shentsize = USHORT
+    e_shnum = USHORT
+    e_shstrnd = USHORT
+
+    def __init__(self, stream: io.RawIOBase, endianness):
+        self.unpack_from_io(stream, endianness)
+
+
+class ElfHeader64(ElfHeader, metaclass=CStruct):
+    e_ident = "16s"
+    e_type = USHORT
+    e_machine = USHORT
+    e_version = UINT
+    e_entry = ULONGLONG
+    e_phoff = ULONGLONG
+    e_shoff = ULONGLONG
+    e_flags = UINT
+    e_ehsize = USHORT
+    e_phentsize = USHORT
+    e_phnum = USHORT
+    e_shentsize = USHORT
+    e_shnum = USHORT
+    e_shstrnd = USHORT
+
+    def __init__(self, stream: io.RawIOBase, endianness):
+        self.unpack_from_io(stream, endianness)
+
+
 class ElfShdr(object):
-    TYPE_NULL = 0x00
-    TYPE_PROGBITS = 0x01
-    TYPE_SYMTAB = 0x02
-    TYPE_STRTAB = 0x03
-    TYPE_RELA = 0x04
-    TYPE_HASH = 0x05
-    TYPE_DYNAMIC = 0x06
-    TYPE_NOTE = 0x07
-    TYPE_NOBITS = 0x08
-    TYPE_REL = 0x09
-    TYPE_SHLIB = 0x0A
-    TYPE_DYNSYM = 0x0B
-    TYPE_LOPROC = 0x70000000
-    TYPE_HIPROC = 0x7FFFFFFF
-    TYPE_LOUSER = 0x80000000
-    TYPE_HIUSER = 0xFFFFFFFF
-
-    FLAGS_WRITE = 0x01
-    FLAGS_ALLOC = 0x02
-    FLAGS_EXECINSTR = 0x04
-    FLAGS_MASKPROC = 0xF0000000
-
-    def __init__(self, stream: io.RawIOBase, header: ElfHeader):
-        self.sh_name = 0
-        self.sh_type = 0
-        self.sh_flags = 0
-        self.sh_addr = 0
-        self.sh_offset = 0
-        self.sh_size = 0
-        self.sh_link = 0
-        self.sh_info = 0
-        self.sh_addralign = 0
-        self.sh_entsize = 0
-
-        if header.wordsz == 32:
-            self.__parse32(stream, header.endianness)
-        elif header.wordsz == 64:
-            self.__parse64(stream, header.endianness)
-
-    def __parse32(self, stream: io.RawIOBase, endianness):
-        self.sh_name = int.from_bytes(stream.read(ELF32_WORD), byteorder=endianness)
-        self.sh_type = int.from_bytes(stream.read(ELF32_WORD), byteorder=endianness)
-        self.sh_flags = int.from_bytes(stream.read(ELF32_WORD), byteorder=endianness)
-        self.sh_addr = int.from_bytes(stream.read(ELF32_ADDR), byteorder=endianness)
-        self.sh_offset = int.from_bytes(stream.read(ELF32_OFF), byteorder=endianness)
-        self.sh_size = int.from_bytes(stream.read(ELF32_WORD), byteorder=endianness)
-        self.sh_link = int.from_bytes(stream.read(ELF32_WORD), byteorder=endianness)
-        self.sh_info = int.from_bytes(stream.read(ELF32_WORD), byteorder=endianness)
-        self.sh_addralign = int.from_bytes(stream.read(ELF32_WORD), byteorder=endianness)
-        self.sh_entsize = int.from_bytes(stream.read(ELF32_WORD), byteorder=endianness)
-
-    def __parse64(self, stream: io.RawIOBase, endianness):
-        self.sh_name = int.from_bytes(stream.read(ELF64_WORD), byteorder=endianness)
-        self.sh_type = int.from_bytes(stream.read(ELF64_WORD), byteorder=endianness)
-        self.sh_flags = int.from_bytes(stream.read(ELF64_XWORD), byteorder=endianness)
-        self.sh_addr = int.from_bytes(stream.read(ELF64_ADDR), byteorder=endianness)
-        self.sh_offset = int.from_bytes(stream.read(ELF64_OFF), byteorder=endianness)
-        self.sh_size = int.from_bytes(stream.read(ELF64_XWORD), byteorder=endianness)
-        self.sh_link = int.from_bytes(stream.read(ELF64_WORD), byteorder=endianness)
-        self.sh_info = int.from_bytes(stream.read(ELF64_WORD), byteorder=endianness)
-        self.sh_addralign = int.from_bytes(stream.read(ELF64_XWORD), byteorder=endianness)
-        self.sh_entsize = int.from_bytes(stream.read(ELF64_XWORD), byteorder=endianness)
+    sh_name = 0
+    sh_type = 0
+    sh_flags = 0
+    sh_addr = 0
+    sh_offset = 0
+    sh_size = 0
+    sh_link = 0
+    sh_info = 0
+    sh_addralign = 0
+    sh_entsize = 0
 
     def type_str(self):
-        val = {ElfShdr.TYPE_NULL: "SHT_NULL",
-               ElfShdr.TYPE_PROGBITS: "SHT_PROGBITS",
-               ElfShdr.TYPE_SYMTAB: "SHT_SYMTAB",
-               ElfShdr.TYPE_STRTAB: "SHT_STRTAB",
-               ElfShdr.TYPE_RELA: "SHT_RELA",
-               ElfShdr.TYPE_HASH: "SHT_HASH",
-               ElfShdr.TYPE_DYNAMIC: "SHT_DYNAMIC",
-               ElfShdr.TYPE_NOTE: "SHT_NOTE",
-               ElfShdr.TYPE_NOBITS: "SHT_NOBITS",
-               ElfShdr.TYPE_REL: "SHT_REL",
-               ElfShdr.TYPE_SHLIB: "SHT_SHLIB",
-               ElfShdr.TYPE_DYNSYM: "SHT_DYNSYM",
-               ElfShdr.TYPE_LOPROC: "SHT_LOPROC",
-               ElfShdr.TYPE_HIPROC: "SHT_HIPROC",
-               ElfShdr.TYPE_LOUSER: "SHT_LOUSER",
-               ElfShdr.TYPE_HIUSER: "SHT_HIUSER"}
+        val = {SHT_TYPE_NULL: "SHT_NULL",
+               SHT_TYPE_PROGBITS: "SHT_PROGBITS",
+               SHT_TYPE_SYMTAB: "SHT_SYMTAB",
+               SHT_TYPE_STRTAB: "SHT_STRTAB",
+               SHT_TYPE_RELA: "SHT_RELA",
+               SHT_TYPE_HASH: "SHT_HASH",
+               SHT_TYPE_DYNAMIC: "SHT_DYNAMIC",
+               SHT_TYPE_NOTE: "SHT_NOTE",
+               SHT_TYPE_NOBITS: "SHT_NOBITS",
+               SHT_TYPE_REL: "SHT_REL",
+               SHT_TYPE_SHLIB: "SHT_SHLIB",
+               SHT_TYPE_DYNSYM: "SHT_DYNSYM",
+               SHT_TYPE_LOPROC: "SHT_LOPROC",
+               SHT_TYPE_HIPROC: "SHT_HIPROC",
+               SHT_TYPE_LOUSER: "SHT_LOUSER",
+               SHT_TYPE_HIUSER: "SHT_HIUSER"}
         return "Unknown: %02x" % self.sh_type if self.sh_type not in val else val[self.sh_type]
 
     def flags_str(self):
         retval = []
-        val = {ElfShdr.FLAGS_WRITE: "SHF_WRITE",
-               ElfShdr.FLAGS_ALLOC: "SHF_ALLOC",
-               ElfShdr.FLAGS_EXECINSTR: "SHF_EXECINSTR",
-               ElfShdr.FLAGS_MASKPROC: "SHF_MASKPROC"}
+        val = {SHT_FLAGS_WRITE: "SHF_WRITE",
+               SHT_FLAGS_ALLOC: "SHF_ALLOC",
+               SHT_FLAGS_EXECINSTR: "SHF_EXECINSTR",
+               SHT_FLAGS_MASKPROC: "SHF_MASKPROC"}
         for key in val:
             if self.sh_flags & key == key:
                 retval.append(val[key])
         return " | ".join(retval)
 
 
+class ElfShdr32(ElfShdr, metaclass=CStruct):
+    sh_name = UINT
+    sh_type = UINT
+    sh_flags = UINT
+    sh_addr = UINT
+    sh_offset = UINT
+    sh_size = UINT
+    sh_link = UINT
+    sh_info = UINT
+    sh_addralign = UINT
+    sh_entsize = UINT
+
+    def __init__(self, stream: io.RawIOBase, endianness):
+        self.unpack_from_io(stream, endianness)
+
+
+class ElfShdr64(ElfShdr, metaclass=CStruct):
+    sh_name = UINT
+    sh_type = UINT
+    sh_flags = ULONGLONG
+    sh_addr = ULONGLONG
+    sh_offset = ULONGLONG
+    sh_size = ULONGLONG
+    sh_link = UINT
+    sh_info = UINT
+    sh_addralign = ULONGLONG
+    sh_entsize = ULONGLONG
+
+    def __init__(self, stream: io.RawIOBase, endianness):
+        self.unpack_from_io(stream, endianness)
+
+
 class Elf:
     def __init__(self, stream: io.RawIOBase):
-        self.header = ElfHeader(stream)
         self.sections = []
         self.shstr = None
+        # Verify ELF and select wordsz and endianness
+        minfo = stream.read(EI_NIDENT)
+        stream.seek(stream.tell() - EI_NIDENT)
+        if ELF_MAGIC not in minfo[0:4]:
+            raise TypeError("Not a valid ELF")
 
-        # Load sections
-        seek = self.header.e_shoff
-        for _ in range(self.header.e_shnum):
-            stream.seek(seek)
-            self.sections.append(ElfShdr(stream, self.header))
-            seek += self.header.e_shentsize
+        if minfo[EI_CLASS] == EI_CLASS32:
+            self.header = ElfHeader32(stream, Elf.__parse_endianness(minfo[EI_DATA]))
+            self.__section_loader(stream, ElfShdr32)
+        elif minfo[EI_CLASS] == EI_CLASS64:
+            self.header = ElfHeader64(stream, Elf.__parse_endianness(minfo[EI_DATA]))
+            self.__section_loader(stream, ElfShdr64)
+        else:
+            raise RuntimeError("Invalid ELF class")
 
         # Load strings table
         stream.seek(self.sections[self.header.e_shstrnd].sh_offset)
@@ -285,6 +276,13 @@ class Elf:
 
     def __str__(self):
         return str(self.header)
+
+    def __section_loader(self, stream: io.RawIOBase, shdr):
+        seek = self.header.e_shoff
+        for _ in range(self.header.e_shnum):
+            stream.seek(seek)
+            self.sections.append(shdr(stream, self.header.endianness))
+            seek += self.header.e_shentsize
 
     def get_section_name(self, section: ElfShdr):
         buf = bytearray()
@@ -297,9 +295,13 @@ class Elf:
         return buf.decode("ascii")
 
     @staticmethod
+    def __parse_endianness(value):
+        return "big" if value == EI_DATA2MSB else "little"
+
+    @staticmethod
     def verify(file: io.RawIOBase):
         s_pos = file.tell()
-        ident = file.read(ElfHeader.NIDENT)
+        ident = file.read(EI_NIDENT)
         ret = ELF_MAGIC in ident[0:4]
         file.seek(s_pos)
         return ret
